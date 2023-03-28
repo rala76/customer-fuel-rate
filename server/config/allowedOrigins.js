27364ae05de1c158f1ad2d782f19f01d95109828
rala76 <71798014+rala76@users.noreply.github.com>
2023-03-28 17:34:38 -0500

    allowedOrigins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 // Who is allowed access to our REST API
 const allowedOrigins = [
     // 'http://localhost:3000',
-    'https://www.customerfuelrate.onrender.com'
+    'https://customerfuelrate.onrender.com'
 ]
 
 module.exports = allowedOrigins
